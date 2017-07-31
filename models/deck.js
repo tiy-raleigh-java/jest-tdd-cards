f83c1378b547e262c8ea355b39e44440db243965
Doug Hughes <doug@doughughes.net>
2017-07-31 11:55:20 -0400

    working deck and card objects with related tests and lots and lots of coverage

--- models/deck.js
+++ models/deck.js
@@ -1,3 +1,27 @@
-function createDeck() {}
+const createCard = require('./card');
+const SUITS = createCard.SUITS;
+const FACE_CARDS = createCard.FACE_CARDS;
+
+// make 52 cards
+let cards = [];
+for (var s = SUITS.CLUBS; s <= SUITS.SPADES; s++) {
+  for (var v = FACE_CARDS.ACE; v <= FACE_CARDS.KING; v++) {
+    cards.push(createCard(v, s));
+  }
+}
+
+function createDeck() {
+  return {
+    cards: cards,
+    shuffle: function() {
+      for (let i = this.cards.length - 1; i >= 0; i--) {
+        let rand = Math.floor(Math.random() * i);
+        // swap the card and this random index
+        // this is called destructuring and is an ES 6 feature.
+        [this.cards[i], this.cards[rand]] = [this.cards[rand], this.cards[i]];
+      }
+    }
+  };
+}
 
 module.exports = createDeck;
